3ade5256cd6f0cb2e3e259cd041cbd7d45664a72
Niño James A. Tan <njtan142@gmail.com>
2021-10-25 11:39:48 +0800

    mockup

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ audio.src = "bg.mp3"
 function play(){
     audio.play()
     var prompt = document.getElementById("prompt")
+    var content = document.getElementById("content")
 
     prompt.style.display = "none"
+    content.style.display = "block"
 }
